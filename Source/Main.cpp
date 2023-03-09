4a4d65e0901fec904c4453c46dfaee820e5ed012
Justus Zorn <84742176+justus-zorn@users.noreply.github.com>
2023-03-09 16:51:09 +0100

    Added dependency on SDL

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,26 @@
 // Copyright 2023 Justus Zorn
 
+#include <SDL.h>
+
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	std::cout << "Hello, world!\n";
+int SDL_main(int argc, char* argv[]) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "SDL initialization failed\n";
+		return 1;
+	}
+	SDL_Window* window = SDL_CreateWindow("My window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+	bool running = true;
+	while (running) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+	SDL_DestroyWindow(window);
+	return 0;
 }
