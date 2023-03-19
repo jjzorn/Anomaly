bad1de38af2c10412382d630f63a08cc67808d53
jjzorn <84742176+jjzorn@users.noreply.github.com>
2023-03-19 22:43:41 +0100

    Reduced dll dependencies on Windows
    - Formatted CMakeLists.txt
    - Set IP to IPv4 loopback

--- Source/Client/Main.cpp
+++ Source/Client/Main.cpp
@@ -10,7 +10,7 @@ int SDL_main(int argc, char* argv[]) {
 	try {
 		Window window;
 		Renderer renderer(window);
-		Client client("2003:dd:ff4f:ea00:19db:908a:2f79:8acc", 17899, window);
+		Client client("127.0.0.1", 17899, window);
 		while (window.update()) {
 			if (!client.update(renderer)) break;
 		}
